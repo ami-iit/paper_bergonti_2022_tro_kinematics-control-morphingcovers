b1ccedaefb1c10ab1f1bb8ecfa5dad8877f09b12
Fabio Bergonti <fabio.bergonti@gmail.com>
2022-07-06 10:44:01 +0200

    Extend PR #7 to `test_ci.m`
    https://github.com/ami-iit/paper_bergonti_2022_tro_kinematics-control-morphingcovers/pull/7

--- scripts/test_ci.m
+++ scripts/test_ci.m
@@ -16,15 +16,20 @@ datasets_full_path = fullfile(fileparts(mfilename('fullpath')),'..','datasets');
 run(fullfile(src_full_path,'setup_sim.m'))
 
 %% Prepare Morphing Cover Model with Motors and its Initial Configuration
+fprintf(' ========= script TEST_CI ========= \n')
+fprintf('- script TEST_CI: start\n')
 
 % 1) create model.
+fprintf('- script TEST_CI: creating model\n')
 model = mystica.model.getModelCoverSquareLinks('n',3,'m',3,'restConfiguration','flat','linkDimension',0.0482);
 % 2) evaluate morphing cover initial configuration.
 % initial configuration is computed running a controlled simulation starting from flat configuration. `mBodyTwist_0` is the control variable.
+fprintf('- script TEST_CI: evaluating initial configuration\n')
 stgs  = mystica.stgs.getDefaultSettingsSimKinAbs(model,'stgs_integrator_limitMaximumTime',4);
 stgs.desiredShape.fun = @(x,y,t) -5*x.^2 -5*y.^2;
 [data,stateKin]  = mystica.runSimKinAbs('model',model,'mBodyPosQuat_0',model.getMBodyPosQuatRestConfiguration,'stgs',stgs,'nameControllerClass','ControllerKinAbs');
 % 3) solve the motors placement problem.
+fprintf('- script TEST_CI: solving motors placement problem\n')
 [model,sensitivity,genAlgrthm] = selectMotorPositioning('model',model,'state',stateKin,'stgs',stgs);
 mBodyPosQuat_0 = data.mBodyPosQuat_0(:,end);
 
@@ -32,6 +37,8 @@ mBodyPosQuat_0 = data.mBodyPosQuat_0(:,end);
 assert(abs(det(stateKin.getZact('model',model)))>1e-5,'selected motor placement doesn''t guarantee full actuation locally')
 
 %% Simulation
+
+fprintf('- script TEST_CI: simulating the kinematics\n')
 
 % stgs: get default values
 stgs = mystica.stgs.getDefaultSettingsSimKinRel(model,'startFile',stgs.saving.workspace.name,'stgs_integrator_limitMaximumTime',6);
@@ -54,3 +61,5 @@ data = mystica.runSimKinRel('model',model,'stgs',stgs,'mBodyPosQuat_0',mBodyPosQ
 % Verify results kinematic simulation
 assert(max(abs(data.errorPositionNormals(:,end)))<1e-2,'errorPositionNormals is bigger than expected')
 assert(max(abs(data.errorOrientationNormals(:,end)))<10,'errorOrientationNormals is bigger than expected')
+
+fprintf(' ========= TEST_CI ended ========= \n')
